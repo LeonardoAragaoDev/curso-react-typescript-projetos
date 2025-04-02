937ead3c5572b99f56661f8dd4fbcf847f7e0b72
Leonardo Aragão <lewoaragao@gmail.com>
2025-04-02 08:23:50 -0300

    ajuste page login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -32,7 +32,7 @@ export default function Login() {
 
   return (
     <div className="flex w-full h-screen items-center justify-center flex-col">
-      <Link to="/" className="mt-11 text-white mb-7">
+      <Link to="/" className="mt-11 text-white mb-7 font-bold text-5xl">
         Dev
         <span className="bg-gradient-to-r from-yellow-500 to-orange-400 bg-clip-text text-transparent">
           Link
