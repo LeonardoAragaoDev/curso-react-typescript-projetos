3fe3b35a5ea28d48445b786adf4a6720b4cfaa7c
Leonardo Aragão <lewoaragao@gmail.com>
2025-04-02 08:18:02 -0300

    ajuste título página home

--- projetos/projeto-linktree/src/pages/home/index.tsx
+++ projetos/projeto-linktree/src/pages/home/index.tsx
@@ -73,7 +73,7 @@ export default function Home() {
   return (
     <div className="flex flex-col w-full py-4 items-center justify-center">
       <h1 className="md:text-4xl text-3xl font-bold text-white mt-20">
-        Página Home!
+        Sujeito Programador
       </h1>
       <span className="text-gray-50 mb-5 mt-3">Veja meus links 👇</span>
 
